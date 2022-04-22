257b9108df7d05814dfcae8354bf9f8924b006fa
AndresLeoFernandez <104107412+AndresLeoFernandez@users.noreply.github.com>
2022-04-22 20:57:39 -0300

    incorporo ej clase 2

---
+++ src/ejercicio2.ts
@@ -0,0 +1,30 @@
+// Imagen numero 1
+console.log("Avanza");
+console.log("Levanta Pila");
+console.log("Avanza");
+console.log("Levantar Pila");
+console.log("Avanzar");
+console.log("Levantar Pila");
+console.log("Avanzar");
+console.log("Levantar Pila");
+console.log("Girar a Izquierda");
+console.log("Avanza");
+console.log("Levanta Pila");
+console.log("Avanza");
+console.log("Levantar Pila");
+console.log("Avanzar");
+console.log("Levantar Pila");
+console.log("Avanzar");
+console.log("Levantar Pila");
+
+// Ejercicio 2
+// Constantes son un valor fijo siempre
+const miConstante: string = "Viernes ";
+// Variables
+let numero_0: number;
+let nombre: string;
+let numero_1: number = 3;
+let numero_2: number = 1;
+// creada e inicializada.
+let resultado: number = numero_1 + numero_2;
+console.log("Este es el resultado: " + resultado + " " + miConstante);
