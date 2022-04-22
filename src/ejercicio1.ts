0a3ce17c10eee3635f19d8a894a67b2eb75deb13
AndresLeoFernandez <104107412+AndresLeoFernandez@users.noreply.github.com>
2022-04-22 19:52:36 -0300

    incorporo ejercicio1

---
+++ src/ejercicio1.ts
@@ -0,0 +1,18 @@
+console.log("Receta de como Hacer una torta de Caja");
+console.log(" 1 -Buscar la caja de la torta.");
+console.log(" 2- 1 (Un) bowl para poner los ingredientes dentro.");
+console.log(" 3- 3 (tres) huevos.");
+console.log(" 4- 200cm cubicos de leche.");
+console.log(" 5- Enmantecar y enharinar un molde de 24 cm.");
+console.log(" 6- Precalentar el hormo al minimo (160ºC) durante 15 minutos.");
+console.log(
+  " 7- Agregar el contenido de la caja en el bowl junto con los huevos y la leche."
+);
+console.log(
+  " 8- Batir con batidora electrica por 3 minutos o con batidor manual por 5"
+);
+console.log(" 9- Pasar la mezcla al molde y llevarla al horno.");
+console.log(
+  "10- Hornear al minimo (160ºC) durante 50 minutos o hasta que introducuendo un palillo en el centro salga seco."
+);
+console.log("deharlo enfriar 15 minutos...");
