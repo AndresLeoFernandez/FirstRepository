275e99b804e552e96cc658e51dbea507e92df381
AndresLeoFernandez <104107412+AndresLeoFernandez@users.noreply.github.com>
2022-04-25 22:17:09 -0300

    ordenamiento del repo por clases y ejercicios

---
+++ clase3/Ejercicio2/src/ejercicio2.ts
@@ -0,0 +1,5 @@
+let palabra_1: String = "Es";
+let palabra_2: String = "una";
+let palabra_3: String = "secuencia";
+let palabra_4: String = "lineal";
+console.log(palabra_1 + palabra_2 + palabra_3 + palabra_4);
