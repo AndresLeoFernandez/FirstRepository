939cbd8a72d8ce27b0fa76aede36291491e4ad82
AndresLeoFernandez <104107412+AndresLeoFernandez@users.noreply.github.com>
2022-05-23 21:58:54 -0300

    ejercicio 4 clase 9

---
+++ Clase9/Ejercicio4/multiplicarArreglo.ts
@@ -0,0 +1,32 @@
+let cargarArreglo = (arreglo: number[], tope: number) => {
+  for (let indice: number = 0; indice < tope; indice++) {
+    arreglo[indice] = Number(
+      prompt(`Indique el nro que va en la posición ${indice}: `)
+    );
+  }
+};
+
+let multiplicarArreglos = (
+  arreglo1: number[],
+  arreglo2: number[],
+  resultado: number[],
+  tope: number
+) => {
+  for (let indice: number = 0; indice < tope; indice++) {
+    resultado[indice] = arreglo1[indice] * arreglo2[indice];
+    console.log(`Multiplicacion[${indice}]= ${resultado[indice]}`);
+  }
+};
+
+let dimensionArreglo: number = Number(
+  prompt(`Ingrese la dimensión del arreglo: `)
+);
+let arregloA: number[] = new Array(dimensionArreglo);
+let arregloB: number[] = new Array(dimensionArreglo);
+let resultadoAxB: number[] = new Array(dimensionArreglo);
+cargarArreglo(arregloA, dimensionArreglo);
+cargarArreglo(arregloB, dimensionArreglo);
+multiplicarArreglos(arregloA, arregloB, resultadoAxB, dimensionArreglo);
+console.log(arregloA);
+console.log(arregloB);
+console.log(resultadoAxB);
